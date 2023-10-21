43b6157c8e75a2c4ee0cd932ebaaf6f1588b8a4a
irynatrysh <98460886+irynatrysh@users.noreply.github.com>
2023-10-21 13:36:41 -0400

    edited hw

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,16 +3,17 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] =
     "live_j4fUgEqkWw8kgwFKev1qg5ycFnQaHFi8PMC6pXPCnXywg3hHpKV6qZweARCxoAaT";
 
+axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 
 const fetchBreeds = () => 
   axios
-    .get('https://api.thecatapi.com/v1/breeds')
+    .get('/breeds')
     .then(response => response.data)
-    .catch(error => console.error(error.status));
+  
 
 const fetchCatByBreed = (breedId) => 
   axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    .get(`/images/search?breed_ids=${breedId}`)
     .then(resp => resp.data)
     .catch(error => console.error(error.status));
 
--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,6 @@ const loader = document.querySelector('.loader');
 const catInfo = document.querySelector('.cat-info');
 const errorText = document.querySelector('.error');
 
-loader.classList.add('is-hidden');
 errorText.classList.add('is-hidden');
 
 function getPetsList(breeds) {
@@ -17,6 +16,7 @@ function getPetsList(breeds) {
     .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
     .join('\n');
 }
+
 fetchBreeds()
   .then(result => {
     getPetsList(result);
@@ -27,38 +27,42 @@ fetchBreeds()
       `Oops! Something went wrong! Try reloading the page!`,
       { timeout: 4000, useIcon: false }
     );
+  })
+  .finally(() => {
+    loader.classList.add('is-hidden');
   });
 
 selector.addEventListener('change', onSelect);
+
 function onSelect(event) {
-  catInfo.classList.add('is-hidden');
-  loader.classList.toggle('is-hidden');
   const selectBreedId = event.currentTarget.value;
+  catInfo.classList.toggle('is-hidden');
 
   fetchCatByBreed(selectBreedId)
     .then(data => {
       markup(data);
       catInfo.classList.toggle('is-hidden');
-      loader.classList.add('is-hidden');
     })
     .catch(() => {
-      catInfo.classList.toggle('is-hidden');
-      loader.classList.toggle('is-hidden');
       Notiflix.Notify.failure(
         `Oops! Something went wrong! Try reloading the page!`,
         { timeout: 4000, useIcon: false }
       );
+    })
+    .finally(() => {
+      loader.classList.add('is-hidden');
     });
 }
+
 function markup(data) {
   const { breeds, url } = data[0];
   const { name, temperament, description } = breeds[0];
   const catCard = `
-   <img src="${url}" alt="${name}" width=500>
+    <img src="${url}" alt="${name}" width=500>
     <div>
-  <h2 class="title">${name}</h2>
-  <p class="text">${description}</p>
-  <p class="text span-text"><span class="span">Temperament:</span> ${temperament}</p>
-</div>`;
+      <h2 class="title">${name}</h2>
+      <p class="text">${description}</p>
+      <p class="text span-text"><span class="span">Temperament:</span> ${temperament}</p>
+    </div>`;
   catInfo.innerHTML = catCard;
 }
